e7ee223fb3779eacfac7e95caf182e39a7c36ed5
Bilalahm02 <bilalahmad02521@gmail.com>
2025-12-26 21:37:17 +0100

    Modification due to determining direction errors
    Working to improve the cameras detection according to expected directions, wind level. This includes new videos/edited videos, and modifying code slightly

--- src/preprocess.py
+++ src/preprocess.py
@@ -14,21 +14,51 @@ SEED = 42
 def label_from_video_folder(folder_name):
     name = folder_name.lower()  # case sensitive handling
 
-    if name.startswith("left"):
-        return "left"
-    if name.startswith("right"):
-        return "right"
-    if name.startswith("away"):
+    # left direction, with strength
+    if name.startswith("leftcalm"):
+        return "left_calm"
+    if name.startswith("leftmoderate"):
+        return "left_moderate"
+    if name.startswith("leftstrong"):
+        return "left_strong"
+
+    # right direction, with strength
+    if name.startswith("rightcalm"):
+        return "right_calm"
+    if name.startswith("rightmoderate"):
+        return "right_moderate"
+    if name.startswith("rightstrong"):
+        return "right_strong"
+
+    # away wind - no strength split needed
+    if name.startswith("awaywind"):
         return "away"
+
+    # Stationary - no wind
     if name.startswith("stationary"):
         return "stationary"
+    
+    # No object / empty
+    if name.startswith("noobject"):
+        return "noObject"
     
     return None
 
 # Ensure dataset directories exist
 def ensure_directories_exist():
+    classes = [
+        "left_calm",
+        "left_moderate",
+        "left_strong",
+        "right_calm",
+        "right_moderate",
+        "right_strong",
+        "away",
+        "stationary",
+        "noobject",        # new
+    ]
     for split in SPLIT.keys():    # train, val, test
-        for cls in ["left", "right", "away", "stationary"]: # class for labels
+        for cls in classes: # class for labels
             (DATASET_DIR / split / cls).mkdir(parents=True, exist_ok=True)  # create directories if they don't exist
 
 # Randomly assign a split based on defined ratios
